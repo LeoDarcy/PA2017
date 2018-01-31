e26d2e5b77114ac0526bf25903d50932aba5eaf7
tracer-pa2017 <tracer@njuics.org>
2018-01-31 23:35:25 +0800

    >  nemu 161220002 bjy Linux Debian 4.9.0-3-686-pae #1 SMP Debian 4.9.30-2+deb9u5 (2017-09-19) i686 GNU/Linux  23:35:25 up 1 day, 22:39,  1 user,  load average: 0.08, 0.57, 0.89 8d191a23f2e65fa4a2b3bb54df8e6c90ec927db3

--- nemu/include/cpu/instr/setcc.h
+++ nemu/include/cpu/instr/setcc.h
@@ -3,5 +3,10 @@
 
 make_instr_func(setne_b);
 make_instr_func(sete_b);
-
+make_instr_func(seto_b);
+make_instr_func(setno_b);
+make_instr_func(setc_b);
+make_instr_func(setae_b);
+make_instr_func(seta_b);
+make_instr_func(setbe_b);
 #endif
